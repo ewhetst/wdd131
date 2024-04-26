3eb7f5c6bf8454b5ce7006c023e17871e674cae8
Eli Whetstone <whe20016@byui.edu>
2024-04-25 20:21:31 -0500

    changes

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const copyWrite = "&copy"
+let lastModDate = document.lastModified
+let currentYear = new Date().getFullYear()
+ 
+document.querySelector("#currentYear").innerHTML = copyWrite + currentYear
+document.querySelector("#lastModified").innerHTML = `Last Modifed: ${lastModDate}`
