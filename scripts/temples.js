23ad9939c48d5f541d188182d4319776e86865c5
Eli Whetstone <whe20016@byui.edu>
2024-05-05 00:07:44 -0500

    temples

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+    const year = document.getElementById('currentyear');
+    year.innerHTML = new Date().getFullYear();
+});
+
+window.addEventListener('load', () => {
+    const modified = document.getElementById('lastModified');
+    modified.innerHTML = document.lastModified;
+});
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
